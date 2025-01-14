a5431c363273a142d8143bffffa2369377e9c59b
Ivan <ivalisleo@alu.edu.gva.es>
2025-01-14 17:03:22 +0100

    Interfaz calculadora terminada

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,7 +3,25 @@ import "../styles/calculator.css";
 export default function calculator(){
     return(
         <div id="calculator">
-            
+            <input type="text" id="inputResultado"/>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>*</button>
+            <button>=</button>
+            <button>0</button>
+            <button>.</button>
+            <button>/</button>
+            <button id="btnBorrar">Borrar</button>
         </div>
     )
+
 }
